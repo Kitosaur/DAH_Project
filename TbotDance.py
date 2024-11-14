e18ae1a12f5171ca8939200104f5c7b20c69053e
MeganMEllis <m.ellis-8@sms.ed.ac.uk>
2024-11-14 14:42:11 +0000

    Update TbotDance.py

--- TbotDance.py
+++ TbotDance.py
@@ -20,39 +20,43 @@ tbot = Trilobot()
 camera = PiCamera()
 
 max_speed = 1         
-goal_distance = 20.0   
+goal_distance = 20.0 #The distance the trilobot will stay away from an object when following it
 speed_range = 5.0  # The distance an object is from the goal that will have the robot drive at full speed
-SPEED = 0.7  # The speed to drive at
+SPEED = 0.7  # The speed the trilobot will move with 
 TURN_DISTANCE = 20  # How close an object needs to be before the robot turns
 FORWARD_DISTANCE = 5  # The distance to move forward before checking for an object again
-TURN_DURATION = 1.0
-FORWARD_DURATION = 0.5
+TURN_DURATION = 1.0 #Time taken for the trilobot to do a full turn
+FORWARD_DURATION = 0.5 #Time the trilobot will move forwards for 
 
 def check_button_press(button):
-    """Check if a specified button is pressed."""
+    """
+    Check if a specified button is pressed.
+    """
     return tbot.read_button(button)
 
 def flash_lights_dance():
     """
     Flashes lights on the Trilobot with a multicoloured pattern and performs a dance routine for 30 seconds.
     """
-    duration = 30
-    flashes = 10
-    flash_interval = 0.3
-    slow_interval = 0.5
-    end_time = time.time() + duration
+    duration = 30 #number of seconds the trilobot will preform for
+    flashes = 10 #number of flashes
+    flash_interval = 0.3 #time between flashes
+    slow_interval = 0.5 #time duration for slower movements
+    end_time = time.time() + duration 
 
     while time.time() < end_time:
         
         for i in range(flashes):
             # Turn on LEDs with a cycling colour pattern
             for led in range(NUM_BUTTONS):
+                #for each flash, a color value (RGB tuple), is calculated by multiplying i by a different factor (25, 50, 75)
+                #and taking the remainder when divided by 255.
                 colour = (i * 25 % 255, (i * 50) % 255, (i * 75) % 255)
                 tbot.set_button_led(led, True, colour=colour)
-            
+            #pauses for the flash interval between flashes
             time.sleep(flash_interval)
             
-            # Dance moves with varied speeds
+            #The dancing
             tbot.forward()          
             time.sleep(flash_interval)
             tbot.backward()          
@@ -69,7 +73,7 @@ def flash_lights_dance():
             time.sleep(flash_interval)
             tbot.coast()
 
-            # Turn LEDs off after each flash cycle
+            #Turn LEDs off after each flash cycle
             for led in range(NUM_BUTTONS):
                 tbot.set_button_led(led, False)
             time.sleep(flash_interval)
@@ -78,17 +82,16 @@ def follow_person():
     """
     Follow a person/object by maintaining a set distance using the ultrasonic sensor on the Trilobot.
     """
-    print("Following mode activated. Press Button B again to stop.")
     while True:
         # Check if Button B is pressed again to stop following
         if check_button_press(BUTTON_B):
-            print("Button B pressed again - stopping follow mode.")
             tbot.disable_motors()
             break
         
         # Measure distance and adjust speed to maintain goal distance
         distance = tbot.read_distance()
-        if distance >= 0.0:  # Valid distance reading
+        if distance >= 0.0:  #Valid distance reading
+            #this line finds the difference between the distance from the object and the ideal distance and normalises it
             scale = (distance - goal_distance) / speed_range
             speed = max(min(scale, 1.0), -1.0) * max_speed
             tbot.set_motor_speeds(speed, speed)
