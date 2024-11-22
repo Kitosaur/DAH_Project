4036893be5f6732e495f0bd896d984b8fb961b3a
MeganMEllis <m.ellis-8@sms.ed.ac.uk>
2024-11-22 15:56:42 +0000

    Distances.py

--- Distances.py
+++ Distances.py
@@ -12,18 +12,25 @@ import matplotlib.pyplot as plt
 tbot = Trilobot()
 
 def record_distance():
+    """
+    Trilobot will use the ultrasonic sensor to measure the distance to the object and return this distance.
+    """
     distance = tbot.read_distance()
     return distance
 
 def main():
-    
+
+    #initialising empty lists for the distances measured by the ultrasonic sensor and the distances entered by the user.
     sensor_distances = []
     user_distances = []
-    
+
+    #Taking 10 measurements of distance which could be in any range and interval. For this experiment a range from 0-100cm and interval of 10cm was used.
     for i in range(10):
         distance = record_distance()
         sensor_distances.append(distance)
+        #lets the user know what distance the sensor measured.
         print(f"Measurement {i+1}: {distance} cm")
+        #Ask the user to enter the corresponding distance they measure. 
         user_distance = float(input("Enter the distance you measured in cm: "))
         user_distances.append(user_distance)
         
